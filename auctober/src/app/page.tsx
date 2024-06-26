c0b659fcedc75b93a6b2b073981e6658c7dff64e
Durelle Abdul <Durelle.abdul.07@gmail.com>
2024-06-26 17:17:50 +0200

    Adding Google authentication using Auth.Js

--- auctober/src/app/page.tsx
+++ auctober/src/app/page.tsx
@@ -3,13 +3,22 @@ import {bids as bidsSchema} from "@/src/db/schema";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {revalidatePath} from "next/cache";
+import {SignIn} from "@/components/sign-in";
+import {SignOut} from "@/components/signout-button";
+import {auth} from "@/auth";
+import UserAvatar from "@/components/UserAvatar";
 
 export default async function HomePage(){
+
+    const session = await auth()
 
     const bids = await database.query.bids.findMany();
 
   return (
       <main className= "container mx-auto py-12">
+          {session ? <SignOut></SignOut> : <SignIn></SignIn>}
+
+
       <form
           action={async (formData: FormData) => {
             "use server"
